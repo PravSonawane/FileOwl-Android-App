f01ef3a1196180ed293b7d357c0331d70811be4a
PravinSonawane <june.pravin@gmail.com>
2016-12-24 18:00:05 -0500

    Launcher icon created. fragment_home.xml created and layout for largest files created

---
+++ app/src/main/java/com/merryapps/diskhero/ui/HomeFragment.java
@@ -0,0 +1,18 @@
+package com.merryapps.diskhero.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Fragment for the home screen.
+ * Displays the scan information.
+ *
+ * @author Pravin Sonawane (june.pravin@gmail.com)
+ * @since v1.0.0
+ */
+
+public class HomeFragment extends Fragment {
+
+    private static final String TAG = "HomeFragment";
+
+
+}
